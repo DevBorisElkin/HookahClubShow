c9fbd6c03e675cc69aab5689888eca5614d2a137
DevBorisElkin <ea.foreverrr@gmail.com>
2022-03-21 23:06:21 +0300

    the logic for the timer is made

--- HookahClubShow/ViewController.swift
+++ HookahClubShow/ViewController.swift
@@ -9,10 +9,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    var timer = Timer()
+    var secondsPassed = 0
+    var totalTime = 0
+    
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let tagButton = sender.tag
+        print(tagButton)
+        timer.invalidate()
+        
+        totalTime = tagButton
+        
+        secondsPassed = 0
+        
+                
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
+        
     }
+    
+    @objc func timerAction() {
+        
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            print(secondsPassed/totalTime)
+        }
+    }
+    
+        
+        
+        
+    
 
 
 }
