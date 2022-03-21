dad72c966a532ccfacaf73e76ca2ae1944a4a16c
DevBorisElkin <ea.foreverrr@gmail.com>
2022-03-21 23:15:43 +0300

    fixed timer logic

--- HookahClubShow/ViewController.swift
+++ HookahClubShow/ViewController.swift
@@ -11,6 +11,7 @@ class ViewController: UIViewController {
 
     
     @IBOutlet weak var progressBar: UIProgressView!
+    
     var timer = Timer()
     var secondsPassed = 0
     var totalTime = 0
@@ -21,7 +22,7 @@ class ViewController: UIViewController {
         timer.invalidate()
         
         totalTime = tagButton
-        
+        progressBar.progress = 0.0
         secondsPassed = 0
         
                 
@@ -33,6 +34,8 @@ class ViewController: UIViewController {
         
         if secondsPassed < totalTime {
             secondsPassed += 1
+            progressBar.progress = Float(secondsPassed)/Float(totalTime)
+            
             print(Float(secondsPassed)/Float(totalTime))
         }
     }
